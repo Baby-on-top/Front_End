cefff33487d2e1fa212eaea694b5e60eeee32d06
Riudiu <lacvert13@gmail.com>
2023-07-15 17:42:22 +0900

    Initial commit 🌳

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          리그닌 🌳
+        </p>
+      </header>
+    </div>
+  );
+}
