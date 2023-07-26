6c4afc3ca5454ec9902426be40436b8fc5d8ed2b
chwangmin <ckm7907cb@gmail.com>
2023-07-27 04:51:11 +0900

    Feat : 폰트 크기, 폰트 타입 수정 추가 #12

--- src/widgets/note/Note3.jsx
+++ src/widgets/note/Note3.jsx
@@ -1,6 +1,5 @@
 import 'remirror/styles/all.css';
 
-import { WysiwygEditor } from '@remirror/react-editors/wysiwyg';
 import React from 'react';
 import css from 'refractor/lang/css.js';
 import javascript from 'refractor/lang/javascript.js';
@@ -9,9 +8,17 @@ import markdown from 'refractor/lang/markdown.js';
 import typescript from 'refractor/lang/typescript.js';
 import { htmlToProsemirrorNode } from 'remirror';
 import { 
+    BulletListExtension,
+    HardBreakExtension,
+    HeadingExtension,
+    LinkExtension,
+    OrderedListExtension,
+    TaskListExtension,
+    ItalicExtension,
+    FontFamilyExtension, 
+    FontSizeExtension,
     HorizontalRuleExtension,
-    CalloutExtension, 
-    BlockquoteExtension, 
+    CalloutExtension,  
     CodeExtension, 
     UnderlineExtension, 
     BoldExtension, 
@@ -25,10 +32,18 @@ import { WebrtcProvider } from 'y-webrtc';
 import * as Y from 'yjs';
 import {
     EditorComponent,
-    FindReplaceComponent,
+    ListButtonGroup,
     Remirror,
+    ToggleItalicButton,
+    CommandButtonGroup,
+    CommandMenuItem,
+    DecreaseFontSizeButton,
+    DropdownButton,
+    IncreaseFontSizeButton,
+    FindReplaceComponent,
     ThemeProvider,
     useCommands,
+    useActive,
     useRemirror,
     ToggleCodeButton,
     Toolbar,
@@ -38,18 +53,25 @@ import {
     ToggleCalloutButton,
 } from '@remirror/react';
 
-
 const ydoc = new Y.Doc();
 // clients connected to the same room-name share document updates
 const provider = new WebrtcProvider('remirror-yjs-demo', ydoc);
 
 const extensions = () => [
-    new BlockquoteExtension({ triggerKey: 'Tab' }), // Change the trigger key to 'Tab'
+    new HardBreakExtension(),
+    new BulletListExtension(),
+    new OrderedListExtension(),
+    new TaskListExtension(),
+    new HeadingExtension(),
+    new LinkExtension(),
+    new ItalicExtension(),
+    new FontFamilyExtension(),
+    new FontSizeExtension({ defaultSize: '16', unit: 'px' }),
+    // new BlockquoteExtension({ triggerKey: 'Tab' }), // Change the trigger key to 'Tab'
     new HorizontalRuleExtension(),
     ...wysiwygPreset(), 
     new FindExtension(),
     new CalloutExtension(),
-    new BlockquoteExtension(),
     new CodeExtension(),
     new BoldExtension(),
     new UnderlineExtension(),
@@ -57,42 +79,100 @@ const extensions = () => [
     new YjsExtension({ getProvider: () => provider }),
     new CodeBlockExtension({
         supportedLanguages: [css, javascript, json, markdown, typescript],
-      }),
+    }),
 ];
 
-const Menu = () => {
-    const { } = useCommands();
+const FONT_SIZES = ['8', '10', '12', '14', '16', '18', '24', '30'];
 
+const FontSizeButtons = () => {
+    const { setFontSize } = useCommands();
+    const { fontSize } = useActive();
+    return (
+        <DropdownButton aria-label='Set font size' icon='fontSize'>
+            {FONT_SIZES.map((size) => (
+                <CommandMenuItem
+                    key={size}
+                    commandName='setFontSize'
+                    onSelect={() => setFontSize(size)}
+                    enabled={setFontSize.enabled(size)}
+                    active={fontSize({ size })}
+                    label={size}
+                    icon={null}
+                    displayDescription={false}
+                />
+            ))}
+        </DropdownButton>
+    );
+};
+
+const FONT_FAMILIES = [
+  ['serif', 'Serif'],
+  ['sans-serif', 'San serif'],
+  ['cursive', 'Cursive'],
+  ['fantasy', 'Fantasy'],
+];
+
+const FontFamilyButtons = () => {
+  const { setFontFamily } = useCommands();
+  const active = useActive();
   return (
-    <>
-        {/* <button onClick={() => toggleBold()}>Bold</button>
-        <button onClick={() => toggleUnderline()}>Underline</button> */}
-    </>
+    <CommandButtonGroup>
+      <DropdownButton aria-label='Font family' icon='text'>
+        {FONT_FAMILIES.map(([fontFamily, label]) => (
+          <CommandMenuItem
+            key={fontFamily}
+            commandName='setFontFamily'
+            onSelect={() => setFontFamily(fontFamily)}
+            enabled={setFontFamily.enabled(fontFamily)}
+            active={active.fontFamily({ fontFamily })}
+            label={<span style={{ fontFamily }}>{label}</span>}
+          />
+        ))}
+      </DropdownButton>
+    </CommandButtonGroup>
   );
 };
-  
 
+// Add this CSS to your styles or stylesheet
+const customStyles = `
+  .Prosemirror ul,
+  .Prosemirror ol {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+  }
+`;
 
 const Note3 = () => {
-  const { manager, state, onChange } = useRemirror({ extensions, stringHandler: 'html', stringHandler: htmlToProsemirrorNode, core: { excludeExtensions: ['history'] } });
+    const { manager, state, onChange } = useRemirror({ extensions, stringHandler: 'html', stringHandler: htmlToProsemirrorNode, core: { excludeExtensions: ['history'] } });
 
-  return (
-    <ThemeProvider>
-        <WysiwygEditor placeholder='Enter text...' />
-        <Remirror manager={manager} initialContent={state} autoFocus autoRender='Enter your text'>
-            <FindReplaceComponent />
-            <EditorComponent />
-            <Toolbar>
-                <ToggleCodeButton />
-                <ToggleBoldButton />
-                <ToggleUnderlineButton/>
-                <ToggleBlockquoteButton/>
-                <ToggleCalloutButton/>
-            </Toolbar>
-            <Menu />
-        </Remirror>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <style>{customStyles}</style>
+            <Remirror manager={manager} initialContent={state} autoFocus onChange={onChange} autoRender='Enter your text'>
+                <FindReplaceComponent />
+                <EditorComponent />
+                <Toolbar>
+                    <CommandButtonGroup>
+                        <DecreaseFontSizeButton />
+                        <FontSizeButtons />
+                        <IncreaseFontSizeButton />
+                    </CommandButtonGroup>
+                    <FontFamilyButtons />
+                    <ToggleCodeButton />
+                    <ToggleBoldButton />
+                    <ToggleUnderlineButton/>
+                    <ToggleItalicButton />
+                    <ListButtonGroup />
+                    <ToggleBlockquoteButton/>
+                    <ToggleCalloutButton/>
+                </Toolbar>
+            </Remirror>
+        </ThemeProvider>
+    );
 };
 
 export default Note3;
