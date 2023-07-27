8d3c46fb55f149050ce32a1243f86070fbaef7f8
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-27 12:33:46 +0900

    Refactor : 채팅 페이지 css 추가 #16

--- src/routes/chat/Chat.jsx
+++ src/routes/chat/Chat.jsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import { useState } from "react";
 import ChatContent from "../../components/chat/ChatContent";
 import ChatHeader from "../../components/chat/ChatHeader";
@@ -6,9 +7,25 @@ export default function Chat() {
     const [isSave,setIsSave] = useState(false);
 
     return (
-        <div>
-            <ChatHeader isSave={isSave} setIsSave={setIsSave}/>
-            <ChatContent isSave={isSave} setIsSave={setIsSave}/>
+        <div
+        css={{
+            display: "flex",
+            justifyContent: "center",
+        }}>
+            <div
+                css={{
+                    margin: '100px 0px',
+                    width: '50%',
+                    height: '50%',
+                    // TODO: shadow 다듬는 것 수정 필요
+                    boxShadow: '2px 2px 2px 2px #9D9D9D',
+                    borderRadius: '15px',
+                }}>
+                <ChatHeader
+                    isSave={isSave} 
+                    setIsSave={setIsSave}/>
+                <ChatContent isSave={isSave} setIsSave={setIsSave}/> 
+            </div>
         </div>
     )
 }
