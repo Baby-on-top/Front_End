3dd18162d238abf8350586a355e6ccc643b2ab7a
wnstn819 <wnstn819@naver.com>
2023-08-03 01:12:16 +0900

    Feat : 워크스페이스 초대기능 추가 #15

--- src/components/lobby/SideNav.jsx
+++ src/components/lobby/SideNav.jsx
@@ -1,229 +1,311 @@
 /** @jsxImportSource @emotion/react */
-import { useEffect, useState, useRef } from 'react';
-import axios from 'axios';
-import muji from '../../assets/muji.jpg';
-import { kakaoUnlink, kakaoInfo } from '../../utils/apis';
-import { useCookies } from 'react-cookie';
-import { useNavigate } from 'react-router-dom';
-import WorkSpaceModal from './WorkSpaceModal';
-import { recoilWorkspaceList, workspaceCheck, SelectedWsIdx, SelectedWsName } from '../../utils/atoms';
-import { useRecoilState } from 'recoil';
-
+import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import muji from "../../assets/muji.jpg";
+import { kakaoUnlink, kakaoInfo } from "../../utils/apis";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import WorkSpaceModal from "./WorkSpaceModal";
+import {
+  recoilWorkspaceList,
+  workspaceCheck,
+  SelectedWsIdx,
+  SelectedWsName,
+} from "../../utils/atoms";
+import { useRecoilState } from "recoil";
+import { motion } from "framer-motion";
+import InviteModal from "./InviteModal";
+import useModal from "../hooks/useModal";
 
 export default function SideNav() {
-    const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-    const REDIRECT_LOGOUT_URI = process.env.REACT_APP_LOGOUT_REDIRECT_URI;
-    const logoutLink = `https://kauth.kakao.com/oauth/logout?client_id=${REST_API_KEY}&logout_redirect_uri=${REDIRECT_LOGOUT_URI}`
-    const SERVER_URL = '/api/workspace'
-    const [cookies] = useCookies(['cookies']);
-    const [profile, setProfile] = useState();
-    const [name, setName] = useState();
-    const [workspaceList, setWorkspaceList] = useState([]);
-    const [isUpdate, setIsUpdate] = useState(false);
-    const navigate = useNavigate();
+  const { modalOpen, close, open } = useModal();
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_LOGOUT_URI = process.env.REACT_APP_LOGOUT_REDIRECT_URI;
+  const logoutLink = `https://kauth.kakao.com/oauth/logout?client_id=${REST_API_KEY}&logout_redirect_uri=${REDIRECT_LOGOUT_URI}`;
+  const SERVER_URL = "/api/workspace";
+  const [cookies] = useCookies(["cookies"]);
+  const [profile, setProfile] = useState();
+  const [name, setName] = useState();
+  const [workspaceList, setWorkspaceList] = useState([]);
+  const [isUpdate, setIsUpdate] = useState(false);
 
+  const navigate = useNavigate();
 
+  async function getUserInfo() {
+    const response = await kakaoInfo(cookies);
+    setName(response.data.data.name);
+    setProfile(response.data.data.profile);
+    console.log(profile);
+  }
 
-    async function getUserInfo() {
-        const response = await kakaoInfo(cookies);
-        setName(response.data.data.name);
-        setProfile(response.data.data.profile);
-        console.log(profile);
-    }
+  const fetchData = async () => {
+    // const response = await axios.get(SERVER_URL)
+    console.log("aaaa");
+    console.log(cookies.accessToken);
+    const response = await axios.get(SERVER_URL, {
+      headers: { Token: cookies.accessToken },
+    });
 
-    const fetchData = async () => {
-        // const response = await axios.get(SERVER_URL)
-        console.log("aaaa");
-        console.log(cookies.accessToken);
-        const response = await axios.get(SERVER_URL,
-            {
-                headers: { Token: cookies.accessToken },
-            })
+    console.log("dddd");
+    console.log(response);
+    setWorkspaceList(response.data.data);
+  };
 
-        console.log("dddd");
-        console.log(response);
-        setWorkspaceList(response.data.data);
-    };
+  // hover 시작
+  const [isHovering, setIsHovering] = useState(false);
 
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
 
-    // hover 시작
-    const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+  // hover 끝
 
-    const handleMouseOver = () => {
-        setIsHovering(true);
-    };
+  // ref
+  const refs = useRef([]);
 
-    const handleMouseOut = () => {
-        setIsHovering(false);
-    };
-    // hover 끝
+  const [wsIdx, setWsIdx] = useRecoilState(SelectedWsIdx);
+  const [wsName, setWsName] = useRecoilState(SelectedWsName);
+  const selIdx = (idx) => {
+    setWsName(refs.current[idx].innerText);
+    setWsIdx(idx);
+  };
 
-    // ref
-    const refs = useRef([]);
+  // useEffect(()=> {
+  //     console.log('refs')
+  //     console.log(refs)
+  //     console.log(refs.current)
+  //     console.log(wsIdx)
+  // },[wsIdx])
 
-    const [ wsIdx, setWsIdx ] = useRecoilState(SelectedWsIdx);
-    const [ wsName, setWsName ] = useRecoilState(SelectedWsName);
-    const selIdx = (idx) => {
-        setWsName(refs.current[idx].innerText)
-        setWsIdx(idx)
-    }
+  useEffect(() => {
+    getUserInfo();
+    fetchData();
+  }, [cookies]);
 
-    // useEffect(()=> {
-    //     console.log('refs')
-    //     console.log(refs)
-    //     console.log(refs.current)
-    //     console.log(wsIdx)
-    // },[wsIdx])
+  useEffect(() => {
+    fetchData();
+  }, [isUpdate]);
 
-    useEffect(() => {
-        getUserInfo();
-        fetchData();
-    }, [cookies])
-
-    useEffect(() => {
-        fetchData();
-    }, [isUpdate])
-
-
-    return (
-        <div className="side-nav"
+  return (
+    <div
+      className="side-nav"
+      css={{
+        width: "250px",
+        height: "100%",
+        background: "white",
+        position: "fixed",
+        float: "left",
+        borderRight: "1px solid black",
+      }}
+    >
+      <div
+        className="side-nav-top"
+        css={{
+          height: "100px",
+          borderBottom: "1px solid black",
+        }}
+      >
+        <img
+          className="muji"
+          src={profile ?? muji}
+          alt="얼굴"
+          css={{
+            width: "100px",
+            height: "100px",
+            float: "left",
+          }}
+        />
+        <div
+          css={{
+            flex: 1,
+            flexDirection: "row",
+            height: "100px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            className="name"
             css={{
-                width: '250px',
-                height: '100%',
-                background: 'white',
-                position: 'fixed',
-                float: 'left',
-                borderRight: '1px solid black',
-            }}>
-            
+              fontSize: "30px",
+              paddingLeft: "150px",
+              paddingTop: "10px",
+            }}
+          >
+            {name}
+          </div>
+          <div css={{ paddingLeft: "135px" }}>
+            <button
+              onClick={() => {
+                window.location.href = logoutLink;
+              }}
+              css={{
+                flex: 1,
+                height: "30px",
+                marginTop: "5px",
+                marginRight: "5px",
+                color: "white",
+                fontSize: "15px",
+                fontWeight: "bold",
+                paddingLeft: "15px",
+                paddingRight: "15px",
+                backgroundColor: "green",
+                border: "none",
+                borderRadius: "10px",
+              }}
+            >
+              +
+            </button>
 
-            <div className="side-nav-top"
-                css={{
-                    height: '100px',
-                    borderBottom: '1px solid black',
-                }}>
-
-                <img className="muji" src={profile ?? muji} alt="얼굴"
-                    css={{
-                        width: '100px',
-                        height: '100px',
-                        float: 'left',
-                    }} />
-                <div css={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    height: '100px',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-
-                    <div className="name"
-                        css={{
-                            fontSize: '30px',
-                            paddingLeft: '150px',
-                            paddingTop: '10px',
-
-                        }}>{name}</div>
-                    <div css={{ paddingLeft: '135px' }}>
-                        <button
-                            onClick={() => {
-                                window.location.href = logoutLink;
-                            }}
-                            css={{
-                                flex: 1,
-                                height: '30px',
-                                marginTop: '5px',
-                                marginRight: '5px',
-                                color: 'white',
-                                fontSize: '15px',
-                                fontWeight: 'bold',
-                                paddingLeft: '15px',
-                                paddingRight: '15px',
-                                backgroundColor: 'green',
-                                border: 'none',
-                                borderRadius: '10px',
-                            }}>
-                            +
-                        </button>
-
-                        <button
-                            onClick={async () => {
-                                const response = await kakaoUnlink(cookies);
-                                if (response.status === 200) {
-                                    navigate('/login');
-                                }
-                            }
-                            }
-                            css={{
-                                flex: 1,
-                                height: '30px',
-                                marginTop: '5px',
-                                marginRight: '5px',
-                                color: 'white',
-                                fontSize: '15px',
-                                fontWeight: 'bold',
-                                paddingLeft: '15px',
-                                paddingRight: '15px',
-                                backgroundColor: 'red',
-                                border: 'none',
-                                borderRadius: '10px',
-                            }}>
-                            x
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <div className="side-nav-mid">
-                <p
-                    css={{
-                        marginLeft: '15px',
-                        fontSize: '25px',
-                        fontWeight: 'bold',
-                    }}>workspace</p>
-            </div>
-
-            <div className="side-nav-end" 
-                css={{
-                    marginLeft: '15px',
-                    fontSize: '20px',                    
-                }}>
-                {workspaceList.map((Workspace) => (
-                    <div key={Workspace.workspaceId} ref={ (el) => (refs.current[Workspace.workspaceId] = el)} onClick={() => selIdx(Workspace.workspaceId)}>
-                        <img className="side-nav-end-image" src={Workspace.workspaceImage} alt="얼굴"
-                            css={{
-                                height: '30px',
-                                /* 25px */
-                                /* width: 100px; */
-                                float: 'left',
-                                marginTop: '3px',
-                                marginLeft: '5px',
-                                /* margin-bottom: 5px; */
-                                marginRight: '10px',
-                                borderRadius: '30%',
-                            }}
-                        />
-                        <p className={isHovering ? "hover" : ""}
-                            onMouseOver={handleMouseOver}
-                            onMouseOut={handleMouseOut}
-                            css={{
-                                backgroundColor: 'rgba(173, 169, 169, 0.3)',
-                                height: '35px',
-                                borderRadius: '5px',
-                            }}
-                        >{Workspace.workspaceName}</p>
-                        
-                    </div>
-                ))
-
+            <button
+              onClick={async () => {
+                const response = await kakaoUnlink(cookies);
+                if (response.status === 200) {
+                  navigate("/login");
                 }
-                {/* <p>baby_on_top</p>
+              }}
+              css={{
+                flex: 1,
+                height: "30px",
+                marginTop: "5px",
+                marginRight: "5px",
+                color: "white",
+                fontSize: "15px",
+                fontWeight: "bold",
+                paddingLeft: "15px",
+                paddingRight: "15px",
+                backgroundColor: "red",
+                border: "none",
+                borderRadius: "10px",
+              }}
+            >
+              x
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="side-nav-mid">
+        <p
+          css={{
+            marginLeft: "15px",
+            fontSize: "25px",
+            fontWeight: "bold",
+          }}
+        >
+          workspace
+        </p>
+      </div>
+
+      <motion.div
+        className="side-nav-end"
+        css={{
+          padding: "0px 10px",
+          fontSize: "20px",
+        }}
+      >
+        {workspaceList.map((Workspace) => (
+          <motion.div
+            key={Workspace.workspaceId}
+            ref={(el) => (refs.current[Workspace.workspaceId] = el)}
+            onClick={() => selIdx(Workspace.workspaceId)}
+            css={{
+              display: "flex",
+              alignItems: "center",
+              borderRadius: "15px",
+              margin: "10px 0px",
+            }}
+          >
+            <motion.img
+              className="side-nav-end-image"
+              src={Workspace.workspaceImage}
+              alt="얼굴"
+              height={50}
+              width={50}
+              css={{
+                float: "left",
+                marginTop: "3px",
+                marginLeft: "5px",
+                /* margin-bottom: 5px; */
+                marginRight: "10px",
+                borderRadius: "30%",
+              }}
+            />
+            <motion.div
+              css={{
+                backgroundColor: "#E3F4F4",
+                borderRadius: "15px",
+                margin: "5px 0px",
+                width: "160px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <motion.p
+                className={isHovering ? "hover" : ""}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                css={{
+                  marginLeft: "10px",
+                  flex: "3",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {Workspace.workspaceName}
+              </motion.p>
+              {Workspace.workspaceId == wsIdx && (
+                <motion.button
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20,
+                  }}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{
+                    scale: 0.8,
+                    rotate: -45,
+                    borderRadius: "100%",
+                  }}
+                  css={{
+                    backgroundColor: "#91C8E4",
+                    borderRadius: "15px",
+                    borderWidth: "0px",
+                    marginRight: "5px",
+                    width: "55px",
+                    height: "55px",
+                  }}
+                  onClick={open}
+                >
+                  초대
+                </motion.button>
+              )}
+            </motion.div>
+          </motion.div>
+        ))}
+        {/* <p>baby_on_top</p>
                 <p>jungle_blue</p>
                 <p>pintos_study</p>
                 <p>프론트엔드 스터디</p>
                 <p>정글 2기</p> */}
-                <WorkSpaceModal isUpdate={isUpdate} setIsUpdate={setIsUpdate} />
-            </div>
-        </div>
-    );
+
+        {modalOpen && (
+          <InviteModal
+            modalOpen={modalOpen}
+            open={open}
+            close={close}
+            workspaceId={wsIdx}
+          />
+        )}
+
+        <WorkSpaceModal isUpdate={isUpdate} setIsUpdate={setIsUpdate} />
+      </motion.div>
+    </div>
+  );
 }
--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { Urls } from './urls';
+import axios from "axios";
+import { Urls } from "./urls";
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
@@ -10,230 +10,214 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 ///User
 export async function kakaoLogin(code) {
-    try {
-        const response = await axios.get(`${Urls.LOGIN}?code=${code}`);
-        if (!response.data || !response.data.data) {
-            throw new Error('User data is not available');
-        }
-        return response.data;
-    } catch (e) {
-        console.error(e);
+  try {
+    const response = await axios.get(`${Urls.LOGIN}?code=${code}`);
+    if (!response.data || !response.data.data) {
+      throw new Error("User data is not available");
     }
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
 export async function kakaoUnlink(cookie) {
-    const SERVER_URL = '/api/member/unlink';
-    try {
-        const response = await axios.get(SERVER_URL,
-            {
-                headers: { Token: cookie.accessToken },
-            }
-        )
-        return response;
-    } catch (e) {
-        console.error(e);
-    }
+  const SERVER_URL = "/api/member/unlink";
+  try {
+    const response = await axios.get(SERVER_URL, {
+      headers: { Token: cookie.accessToken },
+    });
+    return response;
+  } catch (e) {
+    console.error(e);
+  }
 }
 export async function kakaoInfo(cookie) {
-    const SERVER_URL = '/api/member/info';
-    try {
-        const response = await axios.get(SERVER_URL,
-            {
-                headers: { Token: cookie.accessToken },
-            }
-        )
-        return response;
-    } catch (e) {
-        console.error(e);
-    }
+  const SERVER_URL = "/api/member/info";
+  try {
+    const response = await axios.get(SERVER_URL, {
+      headers: { Token: cookie.accessToken },
+    });
+    return response;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-export async function boardCreate(cookie, image, workspaceId, boardName, boardImage) {
-    const url = '/api/board';
-    const header = {
-        Token: cookie.accessToken,
-        "Content-Type": "multipart/form-data",
-    }
-    const info = {
-        'workspaceId': workspaceId,
-        'boardName': boardName,
-        'boardImage': boardImage,
-    }
-    console.log("image");
-    console.log(image);
-    const formData = new FormData();
-    const blob = new Blob([JSON.stringify(info)], { type: "application/json" })
-    formData.append('file', image);
-    formData.append("info", blob);
+export async function boardCreate(
+  cookie,
+  image,
+  workspaceId,
+  boardName,
+  boardImage
+) {
+  const url = "/api/board";
+  const header = {
+    Token: cookie.accessToken,
+    "Content-Type": "multipart/form-data",
+  };
+  const info = {
+    workspaceId: workspaceId,
+    boardName: boardName,
+    boardImage: boardImage,
+  };
+  console.log("image");
+  console.log(image);
+  const formData = new FormData();
+  const blob = new Blob([JSON.stringify(info)], { type: "application/json" });
+  formData.append("file", image);
+  formData.append("info", blob);
 
-    try {
-        const response = await axios({
-            method: "POST",
-            url: url,
-            headers: header,
-            data: formData,
-        })
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const response = await axios({
+      method: "POST",
+      url: url,
+      headers: header,
+      data: formData,
+    });
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
-
 
 export async function inviteWorkspace(accessToken, workspaceId) {
-    const SERVER_URL = '/api/workspace/invite';
-    try {
-        const response = await axios.get(SERVER_URL,
-            {
-                headers: { Token: accessToken },
-                params: { workspaceId: workspaceId },
-            }
-        )
-        return response;
-    } catch (e) {
-        console.error("fail : " + e);
-    }
+  const SERVER_URL = "/api/workspace/invite";
+  try {
+    const response = await axios.get(SERVER_URL, {
+      headers: { Token: accessToken },
+      params: { workspaceId: workspaceId },
+    });
+    return response;
+  } catch (e) {
+    console.error("fail : " + e);
+  }
 }
 
 export async function inviteUrlCreate(workspaceId) {
-    const SERVER_INVITE_URL = '/api/workspace/invite/'
-    const SERVER_URL = '/url';
-    var tDate = new Date();
-    tDate.setMinutes(tDate.getMinutes() + 5);
-    try {
-        const response = await axios.post(SERVER_URL,
-            {
-
-                longUrl: SERVER_INVITE_URL + workspaceId,
-                expiresDate: tDate
-
-            }
-        )
-        return response;
-    } catch (e) {
-        console.error("fail : " + e);
-    }
+  const SERVER_INVITE_URL = "/api/workspace/invite/";
+  const SERVER_URL = "/url";
+  var tDate = new Date();
+  tDate.setMinutes(tDate.getMinutes() + 5);
+  try {
+    const response = await axios.post(SERVER_URL, {
+      longUrl: SERVER_INVITE_URL + workspaceId,
+      expiresDate: tDate,
+    });
+    return response;
+  } catch (e) {
+    console.error("fail : " + e);
+  }
 }
 
 export async function workspaceCreate(cookie, image, workspaceName) {
-    const url = '/api/workspace';
-    const header = {
-        Token: cookie.accessToken,
-        "Content-Type": "multipart/form-data",
-    }
-    const info = {
-        'workspaceName': workspaceName,
+  const url = "/api/workspace";
+  const header = {
+    Token: cookie.accessToken,
+    "Content-Type": "multipart/form-data",
+  };
+  const info = {
+    workspaceName: workspaceName,
+  };
+  console.log("image");
+  console.log(image);
+  const formData = new FormData();
+  const blob = new Blob([JSON.stringify(info)], { type: "application/json" });
+  formData.append("file", image);
+  formData.append("info", blob);
 
-    }
-    console.log("image");
-    console.log(image);
-    const formData = new FormData();
-    const blob = new Blob([JSON.stringify(info)], { type: "application/json" })
-    formData.append("file", image);
-    formData.append("info", blob);
-
-
-    try {
-        const response = await axios({
-            method: "POST",
-            url: url,
-            headers: header,
-            data: formData,
-        })
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const response = await axios({
+      method: "POST",
+      url: url,
+      headers: header,
+      data: formData,
+    });
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export async function workspaceEdit(cookie, image, workspaceName, workspaceId) {
-    const url = '/api/workspace';
-    const header = {
-        // Token: cookie.accessToken,
-        "Content-Type": "multipart/form-data",
-    }
-    const info = {
-        'workspaceName': workspaceName,
-        'workspaceId': workspaceId,
-    }
-    console.log("image");
-    console.log(image);
-    const formData = new FormData();
-    const blob = new Blob([JSON.stringify(info)], { type: "application/json" })
-    formData.append("file", image);
-    formData.append("info", blob);
+  const url = "/api/workspace";
+  const header = {
+    // Token: cookie.accessToken,
+    "Content-Type": "multipart/form-data",
+  };
+  const info = {
+    workspaceName: workspaceName,
+    workspaceId: workspaceId,
+  };
+  console.log("image");
+  console.log(image);
+  const formData = new FormData();
+  const blob = new Blob([JSON.stringify(info)], { type: "application/json" });
+  formData.append("file", image);
+  formData.append("info", blob);
 
-    try {
-        const response = await axios({
-            method: "PUT",
-            url: url,
-            headers: header,
-            data: formData,
-        })
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const response = await axios({
+      method: "PUT",
+      url: url,
+      headers: header,
+      data: formData,
+    });
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export async function boardEdit(image, boardName, boardId) {
-    const url = '/api/board';
-    const header = {
-        "Content-Type": "multipart/form-data",
-    }
-    const info = {
-        'boardId': boardId,
-        'boardName': boardName,
+  const url = "/api/board";
+  const header = {
+    "Content-Type": "multipart/form-data",
+  };
+  const info = {
+    boardId: boardId,
+    boardName: boardName,
+  };
+  console.log("image");
+  console.log(image);
+  const formData = new FormData();
+  const blob = new Blob([JSON.stringify(info)], { type: "application/json" });
+  formData.append("file", image);
+  formData.append("info", blob);
 
-    }
-    console.log("image");
-    console.log(image);
-    const formData = new FormData();
-    const blob = new Blob([JSON.stringify(info)], { type: "application/json" })
-    formData.append("file", image);
-    formData.append("info", blob);
-
-
-    try {
-        const response = await axios({
-            method: "PATCH",
-            url: url,
-            headers: header,
-            data: formData,
-        })
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const response = await axios({
+      method: "PATCH",
+      url: url,
+      headers: header,
+      data: formData,
+    });
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export async function boardDelete(boardIdx) {
-    const url = '/api/board';
+  const url = "/api/board";
 
-    try {
-        const response = await axios.delete(url,
-            {
-                data: { boardId: boardIdx }
-            }
-        )
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    const response = await axios.delete(url, {
+      data: { boardId: boardIdx },
+    });
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export async function workspaceDelete(workspaceIdx) {
-    const url = '/api/workspace';
-    try {
-        const response = await axios.delete(url,
-            {
-                params: { workspaceId: workspaceIdx }
-            }
-        )
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+  const url = "/api/workspace";
+  try {
+    const response = await axios.delete(url, {
+      params: { workspaceId: workspaceIdx },
+    });
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
 }
-
-
-
