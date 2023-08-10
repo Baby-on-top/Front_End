5e7ac5f13966bddc9916fd397e7a937670f17ffe
chwangmin <ckm7907cb@gmail.com>
2023-08-10 22:44:28 +0900

    Feat : tldraw 저장 처리 구현 했습니다 -> but 어디를 사용하고 있는지는 보이지 않음.

--- src/components/tldraw/hooks/useMultiplayerState.js
+++ src/components/tldraw/hooks/useMultiplayerState.js
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useRef } from "react";
-import throttle from "lodash.throttle";
+import { useCallback, useEffect, useState } from "react";
+import { Room } from "@y-presence/client";
 import {
   awareness,
   doc,
@@ -9,68 +9,40 @@ import {
   yShapes,
 } from "../store";
 
+const room = new Room(awareness, {});
+
 export function useMultiplayerState(roomId) {
-  const tldrawRef = useRef();
+  const [appInstance, setAppInstance] = useState(undefined);
+  const [loading, setLoading] = useState(true);
 
   const onMount = useCallback(
     (app) => {
       app.loadRoom(roomId);
       app.pause();
-      tldrawRef.current = app;
-
-      app.replacePageContent(
-        Object.fromEntries(yShapes.entries()),
-        Object.fromEntries(yBindings.entries()),
-        {}
-      );
+      setAppInstance(app);
     },
     [roomId]
   );
 
-  const onChange = useCallback(
-    throttle((app, reason) => {
-      if (
-        reason &&
-        reason.includes("user") &&
-        !reason.includes("session") &&
-        !reason.includes("delete")
-      ) {
-        return;
-      }
-
-      const shapes = app.shapes;
-      const bindings = app.bindings;
-
-      doc.transact(() => {
-        shapes.forEach((shape) => {
-          if (!shape) {
-            return;
-          } else {
-            yShapes.set(shape.id, shape);
-          }
-        });
-        bindings.forEach((binding) => {
-          if (!binding) {
-            return;
-          } else {
-            yBindings.set(binding.id, binding);
-          }
-        });
-      }, tldrawRef.current);
-
-      const keys = shapes.reduce((keys, shape) => {
-        keys.add(shape.id);
-        return keys;
-      }, new Set());
-
-      Array.from(yShapes.keys()).forEach((id) => {
-        if (!keys.has(id)) {
+  const onChangePage = useCallback((app, shapes, bindings) => {
+    undoManager.stopCapturing();
+    doc.transact(() => {
+      Object.entries(shapes).forEach(([id, shape]) => {
+        if (!shape) {
           yShapes.delete(id);
+        } else {
+          yShapes.set(shape.id, shape);
         }
       });
-    }, 50),
-    []
-  );
+      Object.entries(bindings).forEach(([id, binding]) => {
+        if (!binding) {
+          yBindings.delete(id);
+        } else {
+          yBindings.set(binding.id, binding);
+        }
+      });
+    });
+  }, []);
 
   const onUndo = useCallback(() => {
     undoManager.undo();
@@ -81,68 +53,80 @@ export function useMultiplayerState(roomId) {
   }, []);
 
   const onChangePresence = useCallback((app, user) => {
-    awareness.setLocalStateField("tdUser", user);
+    if (!app.room) return;
+    room.setPresence({ id: app.room.userId, tdUser: user });
   }, []);
 
   useEffect(() => {
-    const onChangeAwareness = throttle(() => {
-      const tldraw = tldrawRef.current;
+    if (!appInstance || !room) return;
 
-      if (!tldraw || !tldraw.room) return;
+    const unsubOthers = room.subscribe("others", (users) => {
+      if (!appInstance.room) return;
 
-      const others = Array.from(awareness.getStates().entries())
-        .filter(([key, _]) => key !== awareness.clientID)
-        .map(([_, state]) => state)
-        .filter((user) => user.tdUser !== undefined);
+      const ids = users
+        .filter((user) => user.presence && user.presence.tdUser)
+        .map((user) => user.presence.tdUser.id);
 
-      const ids = others.map((other) => other.tdUser.id);
-
-      Object.values(tldraw.room.users).forEach((user) => {
-        if (user && !ids.includes(user.id) && user.id !== tldraw.room?.userId) {
-          tldraw.removeUser(user.id);
+      Object.values(appInstance.room.users).forEach((user) => {
+        if (
+          user &&
+          !ids.includes(user.id) &&
+          user.id !== appInstance.room.userId
+        ) {
+          appInstance.removeUser(user.id);
         }
       });
 
-      tldraw.updateUsers(others.map((other) => other.tdUser).filter(Boolean));
-    }, 50);
+      appInstance.updateUsers(
+        users
+          .filter((user) => user.presence && user.presence.tdUser)
+          .map((other) => other.presence.tdUser)
+          .filter(Boolean)
+      );
+    });
 
-    awareness.on("change", onChangeAwareness);
-
-    return () => awareness.off("change", onChangeAwareness);
-  }, []);
+    return () => {
+      unsubOthers();
+    };
+  }, [appInstance]);
 
   useEffect(() => {
-    const handleChanges = throttle((event) => {
-      const tldraw = tldrawRef.current;
+    if (!appInstance) return;
 
-      if (!tldraw || event.origin === tldraw) return;
+    function handleDisconnect() {
+      provider.disconnect();
+    }
 
-      tldraw.replacePageContent(
+    window.addEventListener("beforeunload", handleDisconnect);
+
+    function handleChanges() {
+      appInstance.replacePageContent(
         Object.fromEntries(yShapes.entries()),
         Object.fromEntries(yBindings.entries()),
         {}
       );
-    }, 25);
-
-    yShapes.observeDeep(handleChanges);
-
-    return () => yShapes.unobserveDeep(handleChanges);
-  }, []);
-
-  useEffect(() => {
-    function handleDisconnect() {
-      provider.disconnect();
     }
-    window.addEventListener("beforeunload", handleDisconnect);
 
-    return () => window.removeEventListener("beforeunload", handleDisconnect);
-  }, []);
+    async function setup() {
+      yShapes.observeDeep(handleChanges);
+      handleChanges();
+      setLoading(false);
+    }
+
+    setup();
+
+    return () => {
+      window.removeEventListener("beforeunload", handleDisconnect);
+      yShapes.unobserveDeep(handleChanges);
+    };
+  }, [appInstance]);
 
   return {
     onMount,
-    onChange,
+    onChangePage,
     onUndo,
     onRedo,
+    loading,
     onChangePresence,
   };
 }
