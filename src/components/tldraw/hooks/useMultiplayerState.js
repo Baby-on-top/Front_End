6efb9f693b3b9b9b753046cfda170cebdd7fe1dc
mywnajsldkf <mywnajsldkf@gmail.com>
2023-08-11 02:21:53 +0900

    Feat : 그림 쭉 이어지게 그리기

--- src/components/tldraw/hooks/useMultiplayerState.js
+++ src/components/tldraw/hooks/useMultiplayerState.js
@@ -9,12 +9,15 @@ import {
   yShapes,
 } from "../store";
 
+// 멀티플레이어 환경을 설정하고, 사용자들 간의 상호작용을 처리한다.
 const room = new Room(awareness, {});
 
 export function useMultiplayerState(roomId) {
   const [appInstance, setAppInstance] = useState(undefined);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); // 앱이 로딩중인지 상태를 나타낸다.
 
+  // 앱 인스턴스를 마운트할 때 호출한다.
+  // roomId에 해당하는 방을 로드하고 앱 인스턴스를 설정한다.
   const onMount = useCallback(
     (app) => {
       app.loadRoom(roomId);
@@ -24,6 +27,29 @@ export function useMultiplayerState(roomId) {
     [roomId]
   );
 
+  const onChange = useCallback((app) => {
+    const shapes = app.shapes;
+    const bindings = app.bindings;
+    doc.transact(() => {
+      shapes.forEach((shape) => {
+        if (!shape) {
+          return;
+        } else {
+          yShapes.set(shape.id, shape);
+        }
+      });
+      bindings.forEach((binding) => {
+        if (!binding) {
+          return;
+        } else {
+          yBindings.set(binding.id, binding);
+        }
+      });
+    });
+  }, []);
+
+  // 페이지 내용이 변경되었을 때 호출된다.
+  // 변경된 페이지의 모양(shapes)과 바인딩(bidings)을 동기화하여 문서의 상태를 업데이트한다.
   const onChangePage = useCallback((app, shapes, bindings) => {
     undoManager.stopCapturing();
     doc.transact(() => {
@@ -44,19 +70,25 @@ export function useMultiplayerState(roomId) {
     });
   }, []);
 
+  // 실행 취소
   const onUndo = useCallback(() => {
     undoManager.undo();
   }, []);
 
+  // 다시 실행
   const onRedo = useCallback(() => {
     undoManager.redo();
   }, []);
 
+  // 사용자의 존재 상태 변경을 처리한다. -> 사용자가 현재 룸에 있는지 없는지를 나타내는 것 같음
+  // 다른 사용자의 존재 상태가 변경되었을 때, 해당 변경을 처리하여 사용자의 존재 상태를 업데이트한다.
   const onChangePresence = useCallback((app, user) => {
     if (!app.room) return;
     room.setPresence({ id: app.room.userId, tdUser: user });
   }, []);
 
+  // 룸에 발생하는 이벤트와 상태 변경을 구독하고 해지한다.
+  // 다른 사용자의 상태 변경 및 앱 인스터스의 상태 변화를 구독하고 필요할 때 처리한다.
   useEffect(() => {
     if (!appInstance || !room) return;
 
@@ -107,6 +139,7 @@ export function useMultiplayerState(roomId) {
       );
     }
 
+    // 이벤트 리스너를 설정하고 초기 데이터를 동기화한다.
     async function setup() {
       yShapes.observeDeep(handleChanges);
       handleChanges();
@@ -128,5 +161,6 @@ export function useMultiplayerState(roomId) {
     onRedo,
     loading,
     onChangePresence,
+    onChange,
   };
 }
